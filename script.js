94f9f71722e520604bad90b7d60876934a4cbfac
Aigul <sharipova.aig@gmail.com>
2022-06-29 22:31:41 -0400

    function 2 search

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const allRecipes = document.querySelectorAll('.photo');
 
 inputSearch.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase();
+  const wordTwo = event.target.value.toLowerCase();
 
-  allRecipes.forEach(item => {item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
+  allRecipes.forEach(item => {item.querySelector('p').textContent.toLowerCase().includes(word) || item.querySelector('#country').textContent.toLowerCase().includes(wordTwo) ? (item.style.display = 'block') : (item.style.display = 'none'); })
 })
