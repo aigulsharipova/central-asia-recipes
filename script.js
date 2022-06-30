4f448c8fced9b277584558af2f167958bac2bf26
Aigul <sharipova.aig@gmail.com>
2022-06-29 22:13:57 -0400

    functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const inputSearch = document.querySelector('.input-search');
-const allRecipes = document.querySelectorAll('.recipes');
+const inputSearch = document.querySelector('.search');
+const allRecipes = document.querySelectorAll('.photo');
 
 inputSearch.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase();
+  allRecipes.forEach(item => {item.querySelector(p).textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
 })
-allRecipes.forEach(item => {item.querySelector(p).textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
