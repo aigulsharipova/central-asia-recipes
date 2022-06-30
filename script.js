447dcb13065eb66f6bf0723e3c6e39d148ea4cac
Aigul <sharipova.aig@gmail.com>
2022-06-29 22:05:13 -0400

    function forEach

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const allRecipes = document.querySelectorAll('.recipes');
 inputSearch.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase();
 })
+allRecipes.forEach(item => {item.querySelector(p).textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
