0f8433c697d863e1dd24585409dbf2e529637996
Aigul <sharipova.aig@gmail.com>
2022-06-29 21:59:16 -0400

    function

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const inputSearch = document.querySelector('.input-search');
 const allRecipes = document.querySelectorAll('.recipes');
+
+inputSearch.addEventListener('keyup', function(event) {
+  const word = event.target.value.toLowerCase();
+})
