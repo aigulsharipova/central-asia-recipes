c1644c16e7c67dc8f629248fae930177b49fa330
Aigul <sharipova.aig@gmail.com>
2022-06-29 22:15:19 -0400

    item p

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const allRecipes = document.querySelectorAll('.photo');
 
 inputSearch.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase();
-  allRecipes.forEach(item => {item.querySelector(p).textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
+
+  allRecipes.forEach(item => {item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none'); })
 })
